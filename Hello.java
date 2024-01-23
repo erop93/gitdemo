d692d11defd86ba2be7c79c1b371d5bc46151fa9
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-01-23 15:48:14 +0400

    FIX Greeting complete

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		// todo need new Greeting()
-		System.out.println("Hello, World2!");
+		Object msg = new Greeting();
+		System.out.println(msg);
 	}
 }
